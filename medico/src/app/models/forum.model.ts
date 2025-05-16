c97c1d57b8285957bd1fdee5054d5182f3a88510
clan-graziano <159022062+clan-graziano@users.noreply.github.com>
2025-05-16 08:56:46 +0000

    commento

---
+++ medico/src/app/models/forum.model.ts
@@ -0,0 +1,5 @@
+export class Pren   {
+    constructor(public nome: string, public cognome: string, public indirizzo: string, public telefono: string, public email: string, public data_prenotazione: string, public ora_prenotazione: string,){
+
+    }
+}
