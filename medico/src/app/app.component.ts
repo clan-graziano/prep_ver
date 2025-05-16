aa99cea4f20918ac52c469e03b50b04f30057f14
clan-graziano <159022062+clan-graziano@users.noreply.github.com>
2025-05-16 09:47:00 +0000

    cm2

--- medico/src/app/app.component.ts
+++ medico/src/app/app.component.ts
@@ -10,8 +10,10 @@ import { Pren } from './models/forum.model';
 })
 export class AppComponent {
   title = 'medico';
-  vetprenotazioni: Pren[] = []
-salva(){
-
-}
+  vetprenotazioni: Pren[] = [];
+  salva( nome: string, cognome: string, indirizzo: string, telefono: string, email: string, data_prenotazione: string, ora_prenotazione: string,){
+    let nuovaprenotazione = new Pren(nome, cognome, telefono, email, indirizzo, data_prenotazione, ora_prenotazione);
+      this.vetprenotazioni.push(nuovaprenotazione);
+      console.log(this.vetprenotazioni);
+  }
 }
--- medico/src/app/models/forum.model.ts
+++ medico/src/app/models/forum.model.ts
@@ -1,5 +1,5 @@
 export class Pren   {
-    constructor(public nome: string, public cognome: string, public indirizzo: string, public telefono: string, public email: string, public data_prenotazione: string, public ora_prenotazione: string,){
+    constructor(public nome: string, public cognome: string, public indirizzo: string, public telefono: string, public email: string, public data_prenotazione: string, public ora_prenotazione: string){
 
     }
 }
